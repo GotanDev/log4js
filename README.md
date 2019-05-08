c9a6ef5fef07fa2d170077abfc0431aa81df129c
Damien Cuvillier (Gotan) <damien.cuvillier@gotan.io>
2019-05-08 19:18:12 +0200

    V1 - Log4JS - use debugout for file storage. 6 levels setup.

--- README.md
+++ README.md
@@ -14,7 +14,23 @@ Inspired by Log4J java library.
 Simply use ```log.<method>(message)``` method.
  		
 *@param message* can be string or array of strings
- 
+
+> If message is an array, first message will be used as title of following grouped messages
+
+### METHODS
+
+* All log levels : 
+	* trace(message)
+	* debug(message)
+	* info(message)
+	* warn(message)
+	* error(message)
+	* fatal(message)
+* downloadFile() : download file (max 2500 lines)
+* tail(lineCount): get last logs
+* search(term): search for a specific string in logs
+* printLog(): show in console the entire stored log
+* clearFile(): erase all stored logs
 
 ## CONFIGURATION
 
@@ -26,24 +42,29 @@ All are optionnals. Default values are set.
 * ```level```: minimum allowed log level. all message above this level will be ignored.  
 Choose among LEVELS const values: TRACE, DEBUG, INFO, WARN, ERROR, FATAL  
 default: WARN
-* ```file```: External log file to append.  
-⚠ Browser must be launched with right settings to allow file writing.  
-Only compatible with chromium & firefox.   
+* ```file```: Store all logs in a local storage file.   
+Allows to print & download all logs  
 Set null for disabling this feature.  
+Messages in file storage are not formatted  
 default: null
+*live: Display all messages in console.  
+Print messages in browser console.  
+default: false   
+Note that if file & live are not set, log4js is useless ;-)  
+Message will be formatted in console
 * ```prefix```: Prefix before all log message (usefull for log distinguishing)  
 default: ""
 * ```date_format```: date format for displaying message.  
 See [momentJs documentation](https://momentjs.com/docs/#/displaying/).  
 If *momentJS* is not loaded, this parameter is useless.   
-default: "YMd-Hmi"
+default: "YYMMDD-HHmmss"
 * debug: enable Log4JS debug messages  
 default: false
  
 ## DEPENDENCIES
 
 * momentjs is not required but allows to format date 
- 
+* [debugout.js](https://github.com/inorganik/debugout.js) for file outputting
  
 ## FULL EXAMPLE
  
--- src/log4js.js
+++ src/log4js.js
@@ -1,6 +1,8 @@
+'use strict'
+
 /** Browser log management. 
  * Allows to manage all logs displayed in browser console. 
- * Permits, if browser is launched with required configuration, to append these log in an external file. 
+ * Log can
  * 
  * Inspired by Log4J java library. 
  * 
@@ -8,20 +10,24 @@
  * 		Simply use log.<method>(message) method.
  * 		
  * 		@param message can be string or array of strings
+ * 			If message is an array, first message will be used as title of following grouped messages
  * 
- * CONFIGURATION: You can define these constant 
- *		LOG_CONFIG with following values. 
+ * CONFIGURATION: You can define a configuration object with following values. 
  * 			All are optionnals. Default values are set. 
  * 
  *     		level: minimum allowed log level. all message above this level will be ignored. 
  *	 			Choose among LEVELS const values: TRACE, DEBUG, INFO, WARN, ERROR, FATAL
  *				Default : WARN
  *
- * 			file: External log file to append. 
- *				⚠ Browser must be launched with right settings to allow file writing. 
- *				Only compatible with chromium & firefox
- *				set null for disabling this feature. 
+ * 			file: Store all logs in a local storage file. 
+ * 				Allows to print & download all logs
+ *				Set null for disabling this feature. 
  *				default: null
+ *
+ * 			live: Display all messages in console.
+ *				Print messages in browser console. 
+ *				default: false
+ *				Note that if file & live are not set, log4js is useless ;-)
  *
  * 			prefix: Prefix before all log message (usefull for log distinguishing)
  * 				default: ""
@@ -29,25 +35,25 @@
  *			date_format: date format for displaying message. 
  *				see momentJs documentation
  *				if momentJS is not loaded, this parameter is useless
- * 				default: "YMd-Hmi"
+ * 				default: "YYMMDD-HHmmss"
  *
  * 			debug: enable Log4JS debug messages
  * 				default: false
  * 
  * DEPENDENCIES:
  * 		momentjs is not required but allows to format date 
- * 
+ * 		debugout for file outputting
  * 
  * FULL EXAMPLE
  * 
  *		```
- *		const LOG_CONFIG = {
+ *		var log = new Log4JS({
  *			DEBUG: true, 
  *			LEVEL: Log4JS.LEVELS.TRACE, 
- *			FILE: "/var/log/test-log4js.log",
- *			PREFIX: "MyLogger>"
- *		};
- *		var log = new Log4JS();
+ *			FILE: "log4js.log",
+ *			PREFIX: "MyLogger>", 
+ * 			LIVE: true
+ *		});
  *		log.debug("Info message");
  * 		log.info("Grouped messages", "First message", "Second message");
  *		```
@@ -55,145 +61,330 @@
  * @author Damien Cuvillier <damien@gotan.io>
  * @license MIT
  */
-class Log4JS{
-	constructor(){
-		this.LEVELS = Log4JS.LEVELS;
 
-		this._loadDefaultConfig();
 
-		if (typeof LOG_CONFIG == "object") {
-			this._loadConfig(LOG_CONFIG);
-		}
+class Log4JS {
+	/** Create a new logger. 
+	 * @param specificConfiguration (opt) : all settings. 
+	 * @param loggerName (opt) : logger name. 
+	 * 		it allow to have different configuration for distinct logging purpose. 
+	 *		When not set, put instance to "default" value
+	 */
+	constructor(specificConfiguration, loggerName) {
+		try {
+			if (__log4jsInstances == null) {
+				__log4jsInstances = new Array();
+			}
+			/* During building logging instance, put this instance, in '___LAUNCHING_INSTANCE___' global variable
+			 * Usefull for error handling & file logging */
+			__log4jsInstances[Log4JS.LAUNCHING_INSTANCE_IDENTIFIER] = this;
 
-		if (this.CONFIG.DEBUG) {
-			this.trace("LOG4JS instanciated");
-			if (typeof moment != "undefined"){
-				this.debug("MomentJS is not available. Dates will be not displayed in right format");
+			/** Setup config. */
+			this.LEVELS = Log4JS.LEVELS;
+
+			this._loadDefaultConfig();
+
+			if (typeof specificConfiguration == "object") {
+				this._loadConfig(specificConfiguration);
+			}
+
+			if (loggerName == null) {
+				loggerName = "default";
+			}
+			if (!this.CONFIG.LIVE && this.CONFIG.FILE == null) {
+				throw `Log4JS ${loggerName} is useless : you must set either a filename, or set live parameter to true`;
+			}
+
+			if (this.CONFIG.DEBUG) {
+				this.trace("LOG4JS instanciated");
+				if (typeof moment == "undefined") {
+					this.warn("MomentJS is not available. Dates will be not displayed in right format");
+				}
+			}
+			// Manage local filesystem File LOGGING
+			if (this._isFileFeatureActive()) {
+				this.trace("Init filesystem logging")
+				this._outfile = new debugout();
+
+				this._outfile.realTimeLoggingOn = false; // Avoid console.log message (duplicated log4js behavior)
+				this._outfile.useTimestamps = false;// Avoid timestamp in log message (duplicated log4js behavior)
+				this._outfile.recordLogs = true;
+				this._outfile.maxLines = 2000;
+				this._outfile.autoTrim = true; // FIFO
+				this._outfile.tailNumLines = 100;
+				this._outfile.logFilename = this.CONFIG.FILE;
+
+
+				if (this.CONFIG.DEBUG) {
+					this.debug(`Started debugout file storing`);
+				}
+			}
+
+			/** Log4JS Instances management. 
+			 * Usefull for error handling and get logging in any context.
+			 */
+			if (__log4jsInstances[loggerName] != null) {
+				throw new Exception(`Unable to launch 2 logger with the same name '${loggerName}'`);
+			}
+			__log4jsInstances[loggerName] = this;
+		} finally {
+			if (!this._isFileFeatureActive() /* File launching is async && launching instance will be erased after async process */ ) {
+				this._removeLaunchingInstance();
 			}
 		}
 	}
 
 	/** Load configuration default values */
-	_loadDefaultConfig(){
+	_loadDefaultConfig() {
 		this.CONFIG = {
 			LEVEL: this.LEVELS.WARN,
-			FILE: null, 
-			PREFIX: "", 
-			DATE_FORMAT: "YMD-HMi", 
-			DEBUG: false
+			FILE: null, // Do not record all logs
+			PRINT: false,
+			PREFIX: "",
+			DATE_FORMAT: "YYYYMMDD-HHmmss",
+			LIVE: false
 		};
 	}
 
 	/** Load configuration from LOG_CONFIG const. */
-	_loadConfig($config){
+	_loadConfig($config) {
 		if (typeof LOG_CONFIG.level != "undefined") {
 			this.CONFIG.LEVEL = LOG_CONFIG.level;
 		}
+		if (typeof LOG_CONFIG.file != "undefined") {
+			this.CONFIG.FILE = LOG_CONFIG.file;
+		}
+		if (typeof LOG_CONFIG.prefix != "undefined") {
+			this.CONFIG.PREFIX = LOG_CONFIG.prefix;
+		}
+		if (typeof LOG_CONFIG.date_format != "undefined") {
+			this.CONFIG.DATE_FORMAT = LOG_CONFIG.date_format;
+		}
+		if (typeof LOG_CONFIG.debug != "undefined") {
+			this.CONFIG.DEBUG = LOG_CONFIG.debug;
+		}
+		if (typeof LOG_CONFIG.live != "undefined") {
+			this.CONFIG.LIVE = LOG_CONFIG.live;
+		}
 	}
-	
+
 	/** Add a log message in browser console with a managed log level.
 	 * 
-	 * @param $level log level. see all levels in LEVELS constant.
-	 * @param $message : text message. (date & level will be added in log message). 
-	 * 		if message is an array, all message will be grouped in console.
+	 * @param level log level. see all levels in LEVELS constant.
+	 * @param message : text message. (date & level will be added in log message). 
+	 * 		if message is an array, all message will be grouped in console. First item will be used as title
+	 * @param avoidFormat Avoid message formating. Usefull internaly for grouped messages
+	 * 		default: false
+	 * @param onlyConsole Avoid file logging. Usefull for avoiding infinite loop when file logging do not work
+	 * 		default: false
 	 */
-	_log(level, message){
-		if (typeof moment != "undefined"){
-			var date = moment().format(this.CONFIG.DATE_FORMAT);	
+	_log(level, message, avoidFormat, onlyConsole) {
+
+		if (onlyConsole == null) {
+			onlyConsole = false;
+		}
+		if (typeof moment != "undefined") {
+			var date = moment().format(this.CONFIG.DATE_FORMAT);
 		} else {
 			var date = (new Date()).toLocaleString();
 		}
-		
-		
+
+
 		var enabled = level >= this.CONFIG.LEVEL;
 
 		if (!enabled) {
 			return;
 		}
 
-		if (typeof message == "array") {
-			console.group(message[0]);
-			shift(message);
-			for(var i = 0; i < message.length; i++){
-				log(level, message[i]);
+		var levelLabel = Object.keys(this.LEVELS)[level];
+
+		if (typeof message == "object") {
+			var title = `${this.CONFIG.PREFIX}${date} [${levelLabel}] ${message[0]}`;
+			console.group(title);
+			message.shift();
+			for (var i = 0; i < message.length; i++) {
+				this._log(level, message[i], true,true);
 			}
 			console.groupEnd()
+
+			if (this._isFileFeatureActive() && !onlyConsole) {
+				this._appendLogFile(title + "\n\t" + message.join("\n\t"));
+			}
 			return;
 		}
 
-		message = `${this.CONFIG.PREFIX}${date} [${level}] ${message}`;
-		
-		switch(level) {
-			case this.LEVELS.FATAL:
-			case this.LEVELS.ERROR:
-				console.error(message);
-				break;
-			case this.LEVELS.WARN:
-				console.warn(message);
-				break;
-			case  this.LEVELS.INFO:
-				console.info(message);
-				break;
-			case this.LEVELS.DEBUG:
-			case this.LEVELS.TRACE:
-				console.log(message);
-				break;
+		if (avoidFormat == null || avoidFormat == false)  {
+			message = `${this.CONFIG.PREFIX}${date} [${levelLabel}] ${message}`;
+		}
+		if (this.CONFIG.LIVE){
+			switch (level) {
+				case this.LEVELS.FATAL:
+					message = console.error("%c" + message, "background: red; color: yellow; font-size: x-large");
+					break;
+				case this.LEVELS.ERROR:
+					console.error("%c" + message, "font-weight:bold");
+					break;
+				case this.LEVELS.WARN:
+					console.warn("%c" + message, "font-weight:bold;");
+					break;
+				case this.LEVELS.INFO:
+					console.info(message);
+					break;
+				case this.LEVELS.DEBUG:
+					console.log(message);
+					break;
+				case this.LEVELS.TRACE:
+					console.log("%c" + message, "font-size: smaller; color:#888");
+					break;
+			}
 		}
 
 
-		if (this.CONFIG.FILE != null) {
-			appendTextFile(LOG_FILE, message);
-		}
-	}
-	/** Ecrit sur un fichier en local. */
-	_appendTextFile(){
-		makeTextFile = function (text) {
-		    var data = new Blob([text], {type: 'text/plain'});
-
-		    // If we are replacing a previously generated file we need to
-		    // manually revoke the object URL to avoid memory leaks.
-		    if (textFile !== null) {
-		      window.URL.revokeObjectURL(textFile);
-		    }
-
-		    textFile = window.URL.createObjectURL(data);
-
-		    // returns a URL you can use as a href
-		    return textFile;
+		if (this._isFileFeatureActive() && !onlyConsole) {
+			this._appendLogFile(message);
 		}
 	}
 
-	trace(message){
+
+	trace(message) {
 		this._log(this.LEVELS.TRACE, message);
 	}
-	debug(message){
+	debug(message) {
 		this._log(this.LEVELS.DEBUG, message);
 	}
-	info(message){
+	info(message) {
 		this._log(this.LEVELS.INFO, message);
 	}
-	warn(message){
+	warn(message) {
 		this._log(this.LEVELS.WARN, message);
 	}
-	error(message){
+	error(message) {
 		this._log(this.LEVELS.ERROR, message);
 	}
-	fatal(message){
+	fatal(message) {
 		this._log(this.LEVELS.FATAL, message);
+	}
+
+	downloadLogFile(){
+		this._checkDebugout();
+		this._outfile.downloadLog();
+	}
+
+	search(searchTerm){
+		this._checkDebugout();
+		return this._outfile.search(searchTerm);
+	}
+
+	tail(numLines){
+		this._checkDebugout();
+		return this._outfile.tail(numLines);
+	}
+
+	printLog(){
+		this._checkDebugout();
+		console.log(this._outfile.getLog());
+	}
+
+	clearFile(){
+		this._checkDebugout();
+		this._outfile.clear();
+	}
+
+	_checkDebugout(){
+		if(this._outfile == null || !this._outfile instanceof debugout){
+			throw "'file' log4js parameter is null. Log storage is disabled. This feature is not available";
+		}
+	}
+	/** Append message at the end of log file 
+	 * @param message string to add
+	 */
+	_appendLogFile(message) {
+		if (this._outfile instanceof debugout)	{
+			this._log(Log4JS.LEVELS.TRACE,`Wrote a message to log file ${this.CONFIG.FILE}`, false, true);
+			this._outfile.log(message);
+		}
+	}
+	
+
+
+	/** Get loggerInstance 
+	 *  @param loggerName : get logger from this name. 
+	 * 		if null get 'default' logger, or if not available, first one (arbitrary)
+	 *  @return Log4JS
+	 */
+	static getLogInstance(loggerName) {
+		if (__log4jsInstances == null) {
+			throw "Log4JS is not yet instanciated. No instance found";
+		}
+		if (typeof __log4jsInstances != "object") {
+			throw "Log4JS instances are corrupted";
+		}
+		if (Object.keys(__log4jsInstances).length == 0) {
+			throw "Log4JS instances stock is empty";
+		}
+
+		if (loggerName == null) {
+			loggerName = __log4jsInstances["default"] != null ? "default" : Object.keys(__log4jsInstances)[0]
+		}
+
+		if (!__log4jsInstances[loggerName] instanceof Log4JS) {
+			throw new Exception(`Log4JS instance ${loggerName} is corrupted`);
+		}
+
+		return __log4jsInstances[loggerName];
+	}
+
+	/** Remove if exists launching instance after object creation */
+	_removeLaunchingInstance() {
+		if (Log4JS.__getLaunchingInstance() != null) {
+			var newInstances = new Array();
+			var keys = Object.keys(__log4jsInstances);
+			for (var i = 0; i < keys.length; i++) {
+				if (keys[i] != Log4JS.LAUNCHING_INSTANCE_IDENTIFIER) {
+					newInstances[keys[i]] = __log4jsInstances[keys[i]];
+				}
+			};
+			__log4jsInstances = newInstances;
+
+		}
+	}
+	static __getLaunchingInstance() {
+		if (__log4jsInstances == null || Object.keys(__log4jsInstances).indexOf(Log4JS.LAUNCHING_INSTANCE_IDENTIFIER) == -1) {
+			return null;
+		}
+		return Log4JS.getLogInstance(Log4JS.LAUNCHING_INSTANCE_IDENTIFIER);
+	}
+
+	/** If File feature allowed
+	 */
+	_isFileFeatureActive() {
+		return this.CONFIG.FILE != null; 
 	}
 }
 
+/** @var __log4jsInstances Stores all log4js instances. 
+ * it allows to access them in a any context. 
+ */
+var __log4jsInstances = null;
+
+
 // Define all levels
-Object.defineProperty(Log4JS, "LEVELS",{
-    value: {
-    	TRACE: 1, 
-		DEBUG: 2, 
-		INFO: 3, 
-		WARN: 4, 
-		ERROR: 5, 
-		FATAL: 6
-    },
-    writable : false,
-    enumerable : true,
-    configurable : false
+Object.defineProperty(Log4JS, "LEVELS", {
+	value: {
+		TRACE: 0,
+		DEBUG: 1,
+		INFO: 2,
+		WARN: 3,
+		ERROR: 4,
+		FATAL: 5
+	},
+	writable: false,
+	enumerable: true,
+	configurable: false
+});
+Object.defineProperty(Log4JS, "LAUNCHING_INSTANCE_IDENTIFIER", {
+	value: "___LAUNCHING_INSTANCE___",
+	writable: false,
+	enumerable: false,
+	configurable: false
 });
